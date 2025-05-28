1bd8149b56ad623185c04a5e0a567a552ecaaf5e
Nihad Gurban <nihadqurbanli204@gmail.com>
2025-05-28 19:03:58 +0400

    add to basename

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/finalProject-Algorithmica/' : '/', // Adjust base for development
-  server: {
-    port: 3000, // Use a custom port if needed
-  },
+  base: '/finalProject-Algorithmica/', // Ensure this matches your GitHub repository name
 });
