6963fb7c881836ce0bea1f77f070fbbc1d16f506
Nihad Gurban <nihadqurbanli204@gmail.com>
2025-05-28 19:08:11 +0400

    fixed vite-config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/finalProject-Algorithmica/', // Ensure this matches your GitHub repository name
+  base: '/finalProject-Algorithmica/', 
 });
